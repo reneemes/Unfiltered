a4eb73ca58604a25e4a6950815e2f459bc030e78
Cynthia Rincon <cynthiarincon@MacBookPro.lan>
2026-01-22 23:24:07 -0500

    Add auth route and remove separate login/signup pages

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
+const app = express();
 const port = process.env.PORT || 8080;
 
 // Define paths for Express config
@@ -14,6 +15,33 @@ const partialsPath = path.join(__dirname, '../templates/partials');
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+// Landing Page
+app.get("", (req, res) => {
+  res.render("landing"); //res -> Render -> landing Page (landing.hbs)
+});
+
+// Auth page (login + signup)
+app.get("/auth", (req, res) => {
+  res.render("auth");
+});
+
+// Homepage
+app.get("/homepage", (req, res) => {
+  res.render("homepage"); //res -> Render -> homepage (homepage.hbs)
+});
+
+// Resources
+app.get("/resources", (req, res) => {
+  res.render("resources"); //res -> Render -> resources Page (resources.hbs)
+});
+
+// About Us
+app.get("/About", (req, res) => {
+  res.render("about"); //res -> Render -> About Us Page (about.hbs)
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
