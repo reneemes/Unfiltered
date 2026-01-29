9b302dc80a204a785c3f23d23b80e9f6b7cc5748
Elhadji Massow <massow.ndiaye3011@gmail.com>
2026-01-28 21:40:24 -0500

    Design modifications on certain pages and resolution of some conflicts to improve page connectivity.

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ const express = require("express");
 const hbs = require("hbs");
 const cookieParser = require("cookie-parser");
 
-const authRoutes = require("./routes/auth.js");
-const journalRoutes = require("./routes/journal.js");
-const moodRoutes = require("./routes/mood.js");
+// DB Routes
+// const authRoutes = require("./routes/auth.js");
+// const journalRoutes = require("./routes/journal.js");
+// const moodRoutes = require("./routes/mood.js");
+
+//Initializing App
+const app = express();
 
 const auth = require("./middleware/auth.js");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8080;
 
@@ -19,7 +21,6 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
-const app = express();
 // Parse incoming JSON & form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -48,9 +49,13 @@ app.get("/account", (req, res) => {
 });
 
 // Homepage
-app.get("/homepage", auth, (req, res) => {
-  res.render("homepage"); //res -> Render -> homepage (homepage.hbs)
-});
+app.get(
+  "/homepage",
+  /*auth,*/ (req, res) => {
+    //<-- NEED ATTENTION
+    res.render("homepage"); //res -> Render -> homepage (homepage.hbs)
+  },
+);
 
 // JOURNAL ROUTE <---- NEED WORK!!
 app.use((req, res, next) => {
@@ -76,9 +81,13 @@ app.post("/journal", (req, res) => {
 });
 
 // Resources
-app.get("/resources", auth, (req, res) => {
-  res.render("resources"); //res -> Render -> resources Page (resources.hbs)
-});
+app.get(
+  "/resources",
+  /*auth,*/ (req, res) => {
+    // <-- NEED ATTENTION
+    res.render("resources"); //res -> Render -> resources Page (resources.hbs)
+  },
+);
 
 // About Us
 app.get("/about", (req, res) => {
@@ -102,9 +111,9 @@ app.get("/about", (req, res) => {
 app.use(express.static(publicDirectoryPath));
 
 // routes for Journal, Mood, and Login
-app.use("/auth", authRoutes);
-app.use("/journal", journalRoutes);
-app.use("/mood", moodRoutes);
+// app.use("/auth", authRoutes);
+// app.use("/journal", journalRoutes);
+// app.use("/mood", moodRoutes);
 
 // Error handling
 app.use((err, req, res, next) => {
