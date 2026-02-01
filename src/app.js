bf11af6386b120a8733923cb40e57f7887c4d09a
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-02-01 16:28:56 -0500

    Fix: removing dup routes to journals

--- src/app.js
+++ src/app.js
@@ -52,13 +52,13 @@ app.get("/homepage", auth, (req, res) => {
   res.render("homepage"); //res -> Render -> homepage (homepage.hbs)
 });
 
-// JOURNAL ROUTE <---- NEED WORK!!
-app.use((req, res, next) => {
-  req.user = { id: 1 }; // fake logged-in user
-  next();
-});
-
 // THIS ROUTE ALREADY EXISTS DOWN BELOW
+// JOURNAL ROUTE <---- NEED WORK!!
+// app.use((req, res, next) => {
+//   req.user = { id: 1 }; // fake logged-in user
+//   next();
+// });
+
 // app.post("/journal", (req, res) => {
 //   if (!req.user) {
 //     return res.status(401).json({ message: "Not logged in" });
@@ -92,8 +92,6 @@ app.get("/about", (req, res) => {
     ],
   });
 });
-
-
 
 // routes for Journal, Mood, and Login
 app.use("/auth", authRoutes);
